05a78428648712012c7b2778b4c173d2022f4b9d
mzakyfrz17 <mzakyfrz17@gmail.com>
2023-11-30 17:26:45 +0700

    add commit

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,14 @@
+# Build Docker image for backend
+docker build -t docker.pkg.github.com/mzakyfrz17/a433-microservices/karsajobs:latest .
+
+
+# Login to Docker Hub (replace <username-docker> and <password> with your Docker Hub credentials)
+docker login docker.pkg.github.com -u mzakyfrz17 -p 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
+
+
+# Push Docker image to Docker Hub
+docker push docker.pkg.github.com/mzakyfrz17/a433-microservices/karsajobs:latest
+
+
+# Logout from Docker Hub
+docker logout docker.pkg.github.com
